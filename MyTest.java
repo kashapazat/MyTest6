fba9d2e07c12f3d941e13a3a8aba4dfab6e8046a
Azat <kashapazat@gmail.com>
2016-04-30 11:52:37 +0300

    HW6

--- MyTest.java
+++ MyTest.java
@@ -1,37 +1,75 @@
 import org.testng.annotations.*;
-
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import org.openqa.selenium.By;
 import java.util.concurrent.TimeUnit;
 
 public class MyTest extends TestBase {
-    @Test
-    public void testMy() throws Exception {
-        app.navigationHelper.goToHomePage();
+    @DataProvider
+        public Object[][] messageFormsFromXml() {
+                ArrayList<MessageForm> result = new ArrayList<>();
+                try {
+                        File fxmlFile = new File("Message.xml");
+                        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+                        DocumentBuilder documentBuilder = dbFactory.newDocumentBuilder();
+                        Document document = documentBuilder.parse(fxmlFile);
+                        document.getDocumentElement().normalize();
+                        NodeList nList = document.getElementsByTagName("messages");
+                        for (int i = 0; i < nList.getLength(); i++) {
+                               Node node = nList.item(i);
+                                Element element = (Element) node;
+                                result.add((MessageForm) new MessageForm(element.getElementsByTagName("value").item(0).getTextContent()));
+                            }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                Object[][] obj1 = (Object[][]) result.toArray();
+                return obj1;
+            }
 
-        MessageForm message1 = new MessageForm("фотошоп");
-
+                @Test(dataProvider = "messageFormsFromXml")
+        public void openHomeTest(String value) {
+                    app.navigationHelper.goToHomePage();
+                System.out.println(value);
+                MessageForm message1 = new MessageForm(value);
         app.messageHelper.askQuestion(message1);
+    }
 
+    @Test(dataProvider = "messageFormsFromXml")
+        public void commentYourTopicTest(String value) {
         app.navigationHelper.goToForumPage();
-
         app.navigationHelper.goToLinkByXpath("http://soft.softodrom.ru/");
 
-        MessageForm message2 = new MessageForm("Windows Cleaner");
+        System.out.println(value);
+        MessageForm message2 = new MessageForm(value);
         app.messageHelper.sendComment(message2);
+    }
 
+    @Test
+    @Test(dataProvider = "messageFormsFromXml")
+        public void deleteCommentTest(String value) {
         app.navigationHelper.goToAboutPage();
-
         app.navigationHelper.goToForumPage();
-
-        app.navigationHelper.goToLinkByXpath("http://soft.softodrom.ru/%D0%9F%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D1%8B/?ras=13");
-
-        MessageForm message3 = new MessageForm("message1");
+        MessageForm message3 = new MessageForm(value);
         app.messageHelper.sendComment(message3);
+        app.messageHelper.deleteContent("span.glyphicon.glyphicon-trash");
+    }
 
-        app.navigationHelper.goToProfilePage();
-
+    @Test
+    public void logout(){
         app.loginHelper.logout();
-
+    }
     }
 
 
-}
+
+
--- NavigationHelper.java
+++ NavigationHelper.java
@@ -1,3 +1,14 @@
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
 import org.openqa.selenium.By;
 
 public class NavigationHelper extends HelperBase {
